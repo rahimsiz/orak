082e4ad2998ba020cc264ca9efa41f2e48b0b50b
Ramil Badretdinov <rahimsiz@gmail.com>
2013-01-27 02:28:21 +0400

    added installing and testing tool

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from setuptools import setup
+
+
+setup(
+    name = "orak",
+    version = "0.0.1",
+    author = "Ramil Badretdinov",
+    author_email = "rahimsiz@gmail.com",
+    description = (""),
+    license = "",
+    keywords = "example documentation tutorial",
+    url = "",
+    packages=['checkers', 'tirpan', 'tests'],
+    classifiers=[],
+    test_suite='tests'
+)
